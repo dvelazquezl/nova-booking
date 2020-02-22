4a41b6be92bdf28a7ebf4cbe4fb6d1cdc48ea1fe
LizPaola <liz.garcia@fiuni.edu.py>
2020-02-22 17:25:44 -0300

    Agregar modelo cuidad-departamento

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :cities
+  resources :departaments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # defaults to dashboard
