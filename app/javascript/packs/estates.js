4ea7b9e0f0bbfb271d9e599192a6d0c6c24f1e14
alanB-A <gaba30001@gmail.com>
2020-02-26 21:32:28 -0300

    Agregado librerias faltantes

--- app/javascript/packs/estates.js
+++ app/javascript/packs/estates.js
@@ -1,8 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
 import $ from 'jquery';
 
+import initImageCropper from './angle/modules/forms/imagecrop';
+import initSelect2 from './angle/modules/forms/select2';
+import initWizard from './angle/modules/forms/wizard';
+import initXEditable from './angle/modules/forms/xeditable';
+
+$(function() {
+
+    initImageCropper();
+    initSelect2();
+    initWizard();
+    initXEditable();
+});
 // for image preview in_form.html.erb
 $(function() {
     $('#pictureInput').on('change', function(event) {
