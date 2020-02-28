3e10353389d7011d76265a51dc920933abbfe1a4
NelsonDavid59 <fleitasnelson59@gmail.com>
2020-02-28 00:35:35 -0300

    Se Añadió la barra de menú. Se modificó la ruta root.

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # defaults to dashboard
-  root :to => redirect('/singleview')
-
-  # view routes
-  get '/singleview' => 'singleview#index'
+  root :to => redirect('/welcome/index')
 
   # api routes
   get '/api/i18n/:locale' => 'api#i18n'
