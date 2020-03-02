8c438bb8a275b82d6f58126d5a61ccc898a6aaa6
alanB-A <gaba30001@gmail.com>
2020-03-02 19:50:18 -0300

    Eliminados js e imports innecesarios

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,6 @@
 
 //= require components/datepicker
 
-//import $ from "jquery";
-
-import appInit from './angle/app.init.js';
-document.addEventListener('DOMContentLoaded', appInit);
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
