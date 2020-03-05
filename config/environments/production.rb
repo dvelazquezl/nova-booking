d9c2227c987cdda663f8fa782b92aa665ef04f5d
angel-marecos <61218878+angel-marecos@users.noreply.github.com>
2020-03-05 13:53:56 -0300

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      user_name:      'novabooking20@gmail.com',
+      password:       'novabooking2020',
+      domain:         'gmail.com',
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
 end
