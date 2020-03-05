150bc75a4a306acafafe8a07478f6dcd8cb7cfe9
Pamela Tamay <pamela.tamay@fiuni.edu.py>
2020-03-05 09:37:42 -0300

    subiendo archivos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_195745) do
+ActiveRecord::Schema.define(version: 2020_03_03_024910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_03_01_195745) do
     t.datetime "updated_at", null: false
     t.bigint "owner_id", null: false
     t.string "estate_type"
+    t.boolean "status", default: false, null: false
     t.index ["city_id"], name: "index_estates_on_city_id"
     t.index ["owner_id"], name: "index_estates_on_owner_id"
   end
@@ -116,6 +117,11 @@ ActiveRecord::Schema.define(version: 2020_03_01_195745) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
