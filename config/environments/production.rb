398c28b86d78d3c58ac7ded0a731cf335f5130f1
angel-marecos <angel.marecos@fiuni.edu.py>
2020-03-05 16:01:56 -0300

    corrrecion de production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://novabooking-test.herokuapp.com'}
-  congig.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
