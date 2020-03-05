d1e2416dd3e4e387c9cb124bf049c201c60ba757
angel-marecos <angel.marecos@fiuni.edu.py>
2020-03-05 15:47:47 -0300

    configuracion de mail sendgrid

--- config/initializers/setup_mails.rb
+++ config/initializers/setup_mails.rb
@@ -1,10 +1,1 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-    :address              =>  'smtp.sendgrid.net',
-    :port                 =>  '587',
-    :authentication       =>  :plain,
-    :user_name            =>  'angel-marecos',
-    :password             =>  'leirbag27',
-    :domain               =>  'heroku.com',
-    :enable_starttls_auto  =>  true
 }
