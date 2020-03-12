a4c7ea0ff5ae611029591fbbbd39403edc4838ef
rgb401 <ricardo.gonzalez@fiuni.edu.py>
2020-03-12 04:03:46 -0300

    funciones del controlador

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,64 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user! , except: :show
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(owner_params)
+
+    respond_to do |format|
+      if @booking.save
+        format.html { redirect_to @booking, notice: 'La reserva fue creado satifactoriamente.' }
+        format.json { render :show, status: :created, location: @booking }
+      else
+        format.html { render :new }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @booking.update(owner_params)
+        format.html { redirect_to @booking, notice: 'La reserva fue actualizado correctamente.' }
+        format.json { render :show, status: :ok, location: @booking }
+      else
+        format.html { render :edit }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    respond_to do |format|
+      format.html { redirect_to owners_url, notice: 'La reserva fue eliminado satifactoriamente.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:owner).permit(:client_name, :client_email, :date_start, :date_end, :date_creation, :total_amount, :booking_state)
+  end
 end
