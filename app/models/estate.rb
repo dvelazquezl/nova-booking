a62fddaf26bc73857376664ae252ed628e558d82
Adrian Holman <adrian.holman@fiuni.edu.py>
2020-03-12 10:44:09 -0300

    Cambios de fecha movidos a otro banch

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -66,11 +66,4 @@ class Estate < ApplicationRecord
   extend Enumerize
   enumerize :estate_type, in: [:one_apartment, :home, :hotel]
 
-  scope :with_date_gte, ->(ref_date) {
-    where("estates.created_at >= ?", ref_date)
-  }
-
-  scope :with_date_lt, ->(ref_date) {
-    where("estates.created_at >= ?", ref_date)
-  }
 end
