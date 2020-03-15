e2ed13fa7cf9022f8b6840420d89496fd17c03eb
Pamela Tamay <pamela.tamay@fiuni.edu.py>
2020-03-15 16:27:25 -0300

    publicar habitacion

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -35,6 +35,7 @@ class EstatesController < ApplicationController
     if Owner.find_by(user_id: current_user.id)
       @estate = Estate.new
       @estate.owner_id = Owner.find_by(user_id: current_user.id).id
+      @estate.status = false
       @rooms = @estate.rooms.build
       render :new, locals: { rooms: @rooms}
     else
@@ -54,6 +55,7 @@ class EstatesController < ApplicationController
   # POST /estates.json
   def create
     @estate = Estate.new(estate_params)
+    @estate.status = @estate.rooms.any? {|room| room.status == "published"}
 
     respond_to do |format|
       if @estate.save
