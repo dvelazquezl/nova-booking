0ffe04a7ac166db6bc714c5b06eddd46f2bd587e
rgb401 <ricardo.gonzalez@fiuni.edu.py>
2020-03-19 00:02:42 -0300

    helper booking

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -3,12 +3,12 @@ module BookingsHelper
     Room.find(f.object.room_id).room_type.text
   end
   def room_capacity(f)
-  Room.find(f.object.room_id).room_type.text
+  Room.find(f.object.room_id).capacity
   end
   def room_type_for(f)
     Room.find(f.room_id).room_type.text
   end
   def room_capacity_for(f)
-    Room.find(f.room_id).room_type.text
+    Room.find(f.room_id).capacity
   end
 end
