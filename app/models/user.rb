96dac26c192543190b74bec127c5ad3d2c9e1826
Jorge E <jorgeesquivelfernandez5@gmail.com>
2020-03-22 20:36:15 -0400

    NB-71 hacer login con username o email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_one :owner
+
+  attr_writer :login
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication warden_conditions
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
 end
