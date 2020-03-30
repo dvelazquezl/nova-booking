420894849627b9e414066b5e0e9506f18dc21ac9
Adrian Holman <adrian.holman@fiuni.edu.py>
2020-03-30 15:54:11 -0400

    validacion datepicker final
    La validacion esta funcionando como debe, hubo que hacer un pequeño cambio y agregar una gema, pero el estilo que habia antes se perdio

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,9 +3,36 @@
 //= require bootstrap-datepicker
 //= require bootstrap-datepicker/core
 $(function() {
-    $('#with_date_gte, #with_date_lte').datepicker({
-        todayBtn: true,
-        clearBtn: true,
-        todayHighlight: true
-    });
+    var startDate = new Date();
+    var FromEndDate = new Date();
+    var ToEndDate = new Date();
+    
+    ToEndDate.setDate(ToEndDate.getDate()+365);
+    FromEndDate.setDate(FromEndDate.getDate()+365);
+    
+    $('#with_date_gte').datepicker({
+    
+        weekStart: 1,
+        startDate: startDate,
+        endDate: FromEndDate, 
+        autoclose: true
+    })
+        .on('changeDate', function(selected){
+            startDate = new Date(selected.date.valueOf());
+            startDate.setDate(startDate.getDate(new Date(selected.date.valueOf())));
+            $('#with_date_lte').datepicker('setStartDate', startDate);
+        }); 
+    $('#with_date_lte')
+        .datepicker({
+    
+            weekStart: 1,
+            startDate: startDate,
+            endDate: ToEndDate,
+            autoclose: true
+        })
+        .on('changeDate', function(selected){
+            FromEndDate = new Date(selected.date.valueOf());
+            FromEndDate.setDate(FromEndDate.getDate(new Date(selected.date.valueOf())));
+            $('#with_date_gte').datepicker('setEndDate', FromEndDate);
+        });
 });
