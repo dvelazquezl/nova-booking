fc3156fb5562daf13af9c5576c44e172233e7d8e
junior-rojas <junior.rojas@fiuni.edu.py>
2020-04-01 17:41:51 -0400

    Errores solucionados.

--- app/views/welcome/_selects.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('#filterrific_search_query, .component-select').selectize({
-        theme: 'links',
-        maxOptions: 5,
-        sortField: 'text',
-        placeholder: '¿Qué ciudad búscas?'
-    });
-});
