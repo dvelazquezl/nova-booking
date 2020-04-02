7c8a75b9a544671b6342cda96253f8841372b059
enzoM97 <enzomarecos@gmail.com>
2020-04-01 23:07:32 -0400

    NB-81: Fixed bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,34 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def page_not_found
+    respond_to do |format|
+      format.html { render template: 'errors/not_found_error', layout: 'layouts/application', status: 404 }
+      format.all  { render nothing: true, status: 404 }
+    end
+  end
+
+  def server_error
+    respond_to do |format|
+      format.html { render template: 'errors/internal_server_error', layout: 'layouts/error', status: 500 }
+      format.all  { render nothing: true, status: 500}
+    end
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :last_name, :email, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :last_name, :email, :password, :password_confirmation, :current_password])
   end
+
+  #Excepcion de acceso denegado.
+  rescue_from CanCan::AccessDenied do |exception|
+      respond_to do |format|
+        format.json { head :forbidden }
+        format.html { redirect_to welcome_index_path, :alert => 'Acceso denegado.' }
+      end
+  end
+
 end
