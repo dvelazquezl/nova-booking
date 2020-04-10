3d31d0d7a26f1a4f7a4815457b4c1a10962316a7
rgb401 <ricardo.gonzalez@fiuni.edu.py>
2020-04-10 13:32:12 -0400

    enviar correo de solicitud de valoración

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,43 +1,17 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+
+
 env :PATH, ENV['PATH']
+#para pruebas de manera local
 #set :environment, "development"
 set :output, {:error => "log/cron_error.log", :standard => "log/cron_log.log"}
 
 #set :output, '/log/cron_error.log'
 
 
+#every 1.day, at: '08:00 am' do
+#rake "email_booking:request_assess"
+#end
 
-
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-
-
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-
-# Learn more: http://github.com/javan/whenever
-
-
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-
-every 1.day, at: '08:00 am' do
+every 5.minutes do
  rake "email_booking:request_assess"
 end
-
