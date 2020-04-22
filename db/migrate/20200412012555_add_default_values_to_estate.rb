4a6c4753cdf8da5fd59c9439b34d037b37acc560
Angel <angel.marecos@fiuni.edu.py>
2020-04-22 18:18:48 -0400

    estate: migration: valor por defecto 0 de score

--- db/migrate/20200412012555_add_default_values_to_estate.rb
+++ db/migrate/20200412012555_add_default_values_to_estate.rb
@@ -2,5 +2,6 @@ class AddDefaultValuesToEstate < ActiveRecord::Migration[5.2]
   def change
     change_column :estates, :comments_rating_total,:integer, default: 0
     change_column :estates, :cant_comments,:integer, default: 0
+    change_column :estates, :score, :integer, default: 0
   end
 end
