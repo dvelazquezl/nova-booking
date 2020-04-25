206db81c507ed295961c1344b71d12d4adb9e232
Jorge E <jorgeesquivelfernandez5@gmail.com>
2020-04-24 21:27:23 -0400

    scope para traer ofertas del dia de una propiedad

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,10 @@ class Offer < ApplicationRecord
   has_many :offer_details, dependent: :destroy
   accepts_nested_attributes_for :offer_details, allow_destroy: true,
                                 reject_if: :all_blank
+
+  scope :offers_of_the_day, lambda { |from, to|
+    where('((offers.date_end >= ?) and (offers.date_start <= ?)) or
+          ((offers.date_end >= ?) and (offers.date_start <= ?))',
+          to, to, from, from)
+  }
 end
