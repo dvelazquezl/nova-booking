bab910e92466894f1da8227dafccdb73902280a2
Pamela Tamay <pamela.tamay@fiuni.edu.py>
2020-04-27 15:29:43 -0400

    solucionado rails_best_practices

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,5 +3,7 @@ class Offer < ApplicationRecord
   has_many :offer_details, dependent: :destroy
   accepts_nested_attributes_for :offer_details, allow_destroy: true,
                                 reject_if: :all_blank
+  delegate :name, :to => :estate, :prefix => true
+  delegate :images, :to => :estate, :prefix => true
   self.per_page = 5
 end
