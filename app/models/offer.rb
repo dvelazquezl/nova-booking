38dca06064f8064a146f7a377bd799fe2f9c88a8
alanB-A <gaba30001@gmail.com>
2020-04-27 23:36:43 -0400

    se muestra label de unica oferta en propiedad, dependiendo del rango de fechas de la oferta

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -10,4 +10,12 @@ class Offer < ApplicationRecord
   scope :offers_by_owner, -> (current_owner_id) {
     joins(:estate).where('estates.owner_id = ?',current_owner_id)
   }
+  scope :available_for, ->(estate_id, start_date, end_date){
+    Offer.where('estate_id = ? AND date_start >= ? AND date_end <= ?', estate_id , start_date, end_date)
+  }
+
+  def is_available_for?(date_start, date_end)
+    (self.date_start..self.date_end).cover?(date_start) or
+        (self.date_start..self.date_end).cover?(date_end)
+  end
 end
