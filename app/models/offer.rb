a742716c581b143b5023b9956a6f9bd188d87db1
Jorge E <jorgeesquivelfernandez5@gmail.com>
2020-04-28 10:19:39 -0400

    archivo con conflicto

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,10 +3,11 @@ class Offer < ApplicationRecord
   has_many :offer_details, dependent: :destroy
   accepts_nested_attributes_for :offer_details, allow_destroy: true,
                                 reject_if: :all_blank
+  delegate :name, :to => :estate, :prefix => true
+  delegate :images, :to => :estate, :prefix => true
+  self.per_page = 5
 
-  scope :offers_of_the_day, lambda { |from, to|
-    where('((offers.date_end >= ?) and (offers.date_start <= ?)) or
-          ((offers.date_end >= ?) and (offers.date_start <= ?))',
-          to, to, from, from)
+  scope :offers_by_owner, -> (current_owner_id) {
+    joins(:estate).where('estates.owner_id = ?',current_owner_id)
   }
 end
