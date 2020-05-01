414304a07110011f4f0c353e65a17297df2f0246
junior-rojas <junior.rojas@fiuni.edu.py>
2020-05-01 17:54:57 -0400

    Validacion de descuento.

--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -1,6 +1,16 @@
 $(function () {
-    $(".number").on("keypress", function (event) {
+    $(document).on('keypress', '.number', function () {
         let input = $(event.target).val() + (event.charCode - 48);
         return (input >= 1 && input <= 100)
     });
+
+    $(document).on('paste', '.number', function (e) {
+        input = $(this).val() + e.originalEvent.clipboardData.getData('text');
+        if (e.originalEvent.clipboardData.getData('text').match(/[^\d]/))
+            e.preventDefault(); //prevent the default behaviour
+        if (parseInt(input) < 1 || parseInt(input) > 100)
+             e.preventDefault();
+    });
+
 });
+
