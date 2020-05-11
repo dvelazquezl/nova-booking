618714662e145530cc7301bc5e5342ef3eac4897
Pamela Tamay <pamela.tamay@fiuni.edu.py>
2020-05-11 18:17:01 -0400

    filtro de precio con validacion, agregado en welcome.js

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,21 +3,37 @@
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.es
 //= require jquery.easy-autocomplete
 
-  $(function () {
+$(function () {
     let options_easy = {
 
-      url: "/welcome/resources.json",
-    
-      getValue: "name",
-      
-      list: {
-        match: {
-          enabled: true
-        }
-      },
-    
-      theme: "blue-light"
+        url: "/welcome/resources.json",
+
+        getValue: "name",
+
+        list: {
+            match: {
+                enabled: true
+            }
+        },
+
+        theme: "blue-light"
     };
-      $("#with_search_home").easyAutocomplete(options_easy);
-      $("#with_search_results").easyAutocomplete(options_easy);
-  });
+    $("#with_search_home").easyAutocomplete(options_easy);
+    $("#with_search_results").easyAutocomplete(options_easy);
+});
+
+$(function () {
+    if ($("#price_max").val() == '1000000000')
+        $("#price_max").val('');
+    if ($("#price_min").val() == '0')
+        $("#price_min").val('');
+    $(document).on('paste', '.price', function (e) {
+        if (e.originalEvent.clipboardData.getData('text').match(/[^\d]/))
+            e.preventDefault(); //prevent the default behaviour
+    });
+    $(document).on('keypress', '.price', function () {
+        let input = $(event.target).val() + (event.charCode - 48);
+        console.log(input);
+        return (input >= 1);
+    });
+})
