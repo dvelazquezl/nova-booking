67b7e25186549190dcd697792b9ce47e7df539dd
Daniel Velazquez <42827082+danielvelazquezl@users.noreply.github.com>
2020-05-13 09:15:55 -0400

    Revert "Nb 179"

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -41,8 +41,4 @@ module BookingsHelper
   def is_cancellable?(estate)
 		estate.booking_cancelable
   end
-
-  def is_my_booking?(booking)
-    current_user.email == booking.client_email
-  end
 end
