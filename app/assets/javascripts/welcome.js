818922d850182d4b09e0ff4f7866c71d04ba5f3e
Pamela Tamay <pamela.tamay@fiuni.edu.py>
2020-05-13 00:00:56 -0400

    validacion de precio

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,21 +3,46 @@
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.es
 //= require jquery.easy-autocomplete
 
-  $(function () {
+$(function () {
     let options_easy = {
 
-      url: "/welcome/resources.json",
-    
-      getValue: "name",
-      
-      list: {
-        match: {
-          enabled: true
-        }
-      },
-    
-      theme: "blue-light"
+        url: "/welcome/resources.json",
+
+        getValue: "name",
+
+        list: {
+            match: {
+                enabled: true
+            }
+        },
+
+        theme: "blue-light"
     };
     $("#with_search_home").easyAutocomplete(options_easy);
     $("#with_search_results").easyAutocomplete(options_easy);
-  });
+});
+
+//Función para validar entrada de inputs de precio (no permite ingresar ni pegar valores que no sean numericos)
+$(function () {
+    $(".price").on("keyup", function (event) {
+        let input = $(event.target).val();
+        input = input.replace(/[\D\s\._\-]+/g, "");
+        input = input ? parseInt(input, 10) : 0;
+        $(event.target).val(function () {
+            return input == 0 ? '' : input;
+        });
+    });
+
+    $(".price").on("keypress", function (event) {
+        let input = window.Event ? event.which : event.keyCode
+        return (input >= 48 && input <= 57)
+    });
+
+    $(".price").on("paste", function (e) {
+        let input = $(this).val() + e.originalEvent.clipboardData.getData('text');
+        if (e.originalEvent.clipboardData.getData('text').match(/[^\d]/))
+            e.preventDefault(); //prevent the default behaviour
+        if (parseInt(input) < 1)
+            e.preventDefault();
+    });
+});
