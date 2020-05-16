e942e3490a7b768e61060edd7cfcb33bd5615408
rgb401 <ricardo.gonzalez@fiuni.edu.py>
2020-05-15 20:15:00 -0400

    cambio en caracter permitido

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -6,7 +6,7 @@ class Owner < ApplicationRecord
   validates_length_of :phone, minimum: 5, maximum: 20, allow_blank: true
   validates :phone,numericality: { only_integer: true }
   validates_format_of :address, with: /\A[a-zA-Z0-9!|’'"-\.\/_\s]+\z/
-  validates_format_of :about, with: /\A[a-zA-Z0-9\[\]{}\\*:;@$%&#?!|’'"-\.\/_\s]+\z/
+  validates_format_of :about, with: /\A[a-zA-Z0-9\[\]{}\\*:;@$%&#?!¡|’'"-\.\/_\s]+\z/
 
   resourcify
 end
