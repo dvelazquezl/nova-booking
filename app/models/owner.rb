f3baa399ce7af3fec3af47865ab14f3eeef1ad7e
rgb401 <ricardo.gonzalez@fiuni.edu.py>
2020-05-16 20:22:26 -0400

    validaciones owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,8 +5,8 @@ class Owner < ApplicationRecord
   validates :user_id, uniqueness: true
   validates_length_of :phone, minimum: 5, maximum: 20, allow_blank: true
   validates :phone,numericality: { only_integer: true }
-  validates_format_of :address, with: /\A[a-zA-Z0-9!|’'"-\.\/_\s]+\z/
-  validates_format_of :about, with: /\A[a-zA-Z0-9\[\]{}\\*:;@$%&#?!¡|’'"-\.\/_\s]+\z/
+  validates_format_of :address, with: /\A[a-zA-Z0-9’'"-\.\/_\u00E0-\u00FC\s]+\z/
+  validates_format_of :about, with: /\A[a-zA-Z0-9\[\]{}\\*:;@$%&#?!¡|’'"-\.\/_\u00E0-\u00FC\s]+\z/
 
   resourcify
 end
