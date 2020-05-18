113da949208a5d30820b1733d71e2148d86c3f76
enzoM97 <31116070+enzoM97@users.noreply.github.com>
2020-05-17 21:46:03 -0400

    Delete 20200501220050_create_cancellation_motives.rb

--- db/migrate/20200501220050_create_cancellation_motives.rb
+++
@@ -1,9 +0,0 @@
-class CreateCancellationMotives < ActiveRecord::Migration[5.2]
-  def change
-    create_table :cancellation_motives do |t|
-      t.string :description
-
-      t.timestamps
-    end
-  end
-end
