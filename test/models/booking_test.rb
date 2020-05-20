e70944d9a8c8e01787a9331fcee9c62d38a1efcd
alanB-A <gaba30001@gmail.com>
2020-05-19 23:29:10 -0400

    errores de variables corregidos

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
   test 'should save booking with correct data' do
-    booking = bookings(:correct_data_2)
+    booking = bookings(:correct_data)
     assert booking.save
   end
 
   test 'should bring bookings by client email' do
-    booking = bookings(:correct_data_2)
+    booking = bookings(:correct_data)
     booking.save
     searched_booking = Booking.bookings_by_client(booking.client_email).first
     assert_equal searched_booking, booking
   end
 
   test 'should bring correct booking for request access' do
-    booking = bookings(:correct_data_2)
+    booking = bookings(:correct_data)
     booking.save
     searched_booking = Booking.request_assess.first
     assert_equal searched_booking, booking
