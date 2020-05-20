4f58e2ffacfa82021647da5b2982fc30b2cf1868
alexandra-lezcano <alexandra.lezcano@fiuni.edu.py>
2020-05-19 21:48:43 -0400

    permitir espacios en los nombres y agregar validacion de presence

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,7 +2,7 @@ class City < ApplicationRecord
   belongs_to :departament
   has_many :estates
   delegate :name, :to => :departament, :prefix => true
-  validates :name, format: { with: /\A[a-zA-ZÀ-ÿ]+\z/ }
+  validates :name, presence: true, format: { with: /\A[a-zA-ZÀ-ÿ\-\s]{2,50}\z/i }
 
   self.per_page = 5
   resourcify
--- app/models/departament.rb
+++ app/models/departament.rb
@@ -1,6 +1,6 @@
 class Departament < ApplicationRecord
   has_many :cities
-  validates :name, format: { with: /\A[a-zA-ZÀ-ÿ]+\z/ }
+  validates :name, presence: true, format: { with: /\A[a-zA-ZÀ-ÿ\-\s]{2,50}\z/i }
 
   self.per_page = 5
   resourcify
