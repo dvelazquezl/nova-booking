784bd75a8e17e81200652d44ff30663095b98531
alexandra-lezcano <alexandra.lezcano@fiuni.edu.py>
2020-05-19 21:13:40 -0400

    Revert "test de cities y departments"
    This reverts commit 930cb66617d371ac442a39b95addd803e125aabc.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,8 +2,6 @@ class City < ApplicationRecord
   belongs_to :departament
   has_many :estates
   delegate :name, :to => :departament, :prefix => true
-  validates :name, format: { with: /\A[a-zA-ZÀ-ÿ]+\z/ }
-
   self.per_page = 5
   resourcify
 end
--- app/models/departament.rb
+++ app/models/departament.rb
@@ -1,7 +1,5 @@
 class Departament < ApplicationRecord
   has_many :cities
-  validates :name, format: { with: /\A[a-zA-ZÀ-ÿ]+\z/ }
-
   self.per_page = 5
   resourcify
 end
